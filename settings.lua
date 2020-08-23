6ec6b7c176da1566b1a9e37c1d709fcb6e736c07
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2020-08-23 15:34:21 +0000

    add notes & fix default setting

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@ data:extend(
         {
             name = "science_off-time_limit",
             type = "int-setting",
-            default_value = 60,
+            default_value = 0,
             minimum_value = 0,
             setting_type = "startup",
             order = "1001"
