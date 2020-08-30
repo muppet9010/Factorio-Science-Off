6c0b0eff46346b73683d1a4e3cb9c529ab8c1ede
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2020-08-30 20:26:24 +0000

    Validate Target Run Data setting and handle blank values correctly

--- scripts/target_run_data.lua
+++ scripts/target_run_data.lua
@@ -1,6 +1,6 @@
 local TargetRunData = {}
 local EventScheduler = require("utility/event-scheduler")
---local Logging = require("utility/logging")
+local Logging = require("utility/logging")
 local Interfaces = require("utility/interfaces")
 local Events = require("utility/events")
 local CommonFunctions = require("scripts/common_functions")
@@ -18,24 +18,87 @@ TargetRunData.OnLoad = function()
 end
 
 TargetRunData.OnStartup = function()
-    if global.targetRunData.productionTargetItemCount == 1 then -- protect against second run of ONStartup. We don't support modifying values mid map.
-        TargetRunData.HandleRawTargetRunData(game.json_to_table(settings.startup["science_off-target_run_data"].value))
+    if global.targetRunData.productionTargetItemCount == 1 then -- protect against second run of OnStartup. We don't need to support modifying values mid map.
+        TargetRunData.HandleRawTargetRunData(settings.startup["science_off-target_run_data"].value)
     end
 end
 
-TargetRunData.HandleRawTargetRunData = function(rawData)
+TargetRunData.HandleRawTargetRunData = function(settingString)
+    if settingString == nil or settingString == "" then
+        return
+    end
+
     local data = global.targetRunData.data
-    data.scienceUsedTotal = rawData.scienceUsedTotal
-    data.pointsTotal = rawData.pointsTotal
-    data.endTimeTick = rawData.endTimeTick
+    local validSciencePackPrototypes = game.get_filtered_item_prototypes({{filter = "subgroup", subgroup = "science-pack"}})
+
+    local rawData = game.json_to_table(settingString)
+    if rawData == nil or type(rawData) ~= "table" then
+        return TargetRunData.ValidationFailed("invalid data structure")
+    end
+
+    if rawData.scienceUsedTotal == nil or type(rawData.scienceUsedTotal) ~= "table" then
+        return TargetRunData.ValidationFailed("scienceUsedTotal is invalid data structure")
+    end
+    if Utils.GetTableNonNilLength(rawData.scienceUsedTotal) == 0 then
+        return TargetRunData.ValidationFailed("scienceUsedTotal is empty list")
+    end
+    data.scienceUsedTotal = {}
+    for sciencePackName, rawCount in pairs(rawData.scienceUsedTotal) do
+        local count = tonumber(rawCount)
+        if count == nil or count < 0 then
+            return TargetRunData.ValidationFailed("scienceUsedTotal count not a non-negative number:'" .. tostring(rawCount) .. "'")
+        end
+        if validSciencePackPrototypes[sciencePackName] == nil then
+            return TargetRunData.ValidationFailed("scienceUsedTotal science pack type not valid:'" .. tostring(sciencePackName) .. "'")
+        end
+        data.scienceUsedTotal[sciencePackName] = count
+    end
+
+    data.pointsTotal = tonumber(rawData.pointsTotal)
+    if rawData.pointsTotal == nil or rawData.pointsTotal < 0 then
+        return TargetRunData.ValidationFailed("pointsTotal not a non-negative number: '" .. tostring(rawData.pointsTotal) .. "'")
+    end
+
+    data.endTimeTick = tonumber(rawData.endTimeTick)
+    if rawData.endTimeTick == nil or rawData.endTimeTick < 0 then
+        return TargetRunData.ValidationFailed("endTimeTick not a non-negative number: '" .. tostring(rawData.endTimeTick) .. "'")
+    end
+
+    if rawData.scienceUsedHistory == nil or type(rawData.scienceUsedHistory) ~= "table" then
+        return TargetRunData.ValidationFailed("scienceUsedHistory is invalid data structure")
+    end
+    if Utils.GetTableNonNilLength(rawData.scienceUsedHistory) == 0 then
+        return TargetRunData.ValidationFailed("scienceUsedHistory is empty list")
+    end
     data.scienceUsedHistoryEntries = {}
     local currentTargetItemEntry = 1
-    for tick, sciencePacksUsed in pairs(rawData.scienceUsedHistory) do
-        tick = tonumber(tick)
+    for rawTick, rawSciencePacksUsed in pairs(rawData.scienceUsedHistory) do
+        local tick = tonumber(rawTick)
+        if tick == nil or tick <= 0 then
+            return TargetRunData.ValidationFailed("scienceUsedHistory entry tick not a positive number:'" .. tostring(rawTick) .. "'")
+        end
+        local sciencePacksUsed = {}
+        for sciencePackName, rawCount in pairs(rawSciencePacksUsed) do
+            local count = tonumber(rawCount)
+            if count == nil or count < 0 then
+                return TargetRunData.ValidationFailed("scienceUsedHistory count not a non-negative number:'" .. tostring(rawCount) .. "' for tick " .. tick)
+            end
+            if validSciencePackPrototypes[sciencePackName] == nil then
+                return TargetRunData.ValidationFailed("scienceUsedHistory science pack type not valid:'" .. tostring(sciencePackName) .. "' for tick " .. tick)
+            end
+            sciencePacksUsed[sciencePackName] = count
+        end
         data.scienceUsedHistoryEntries[currentTargetItemEntry] = {tick = tick, sciencePacksUsed = sciencePacksUsed}
         currentTargetItemEntry = currentTargetItemEntry + 1
     end
+
     TargetRunData.RegisterDataEvent()
+end
+
+TargetRunData.ValidationFailed = function(specificMessage)
+    local errorMessage = "ERROR: bad Target Run Data setting provided: " .. specificMessage
+    Logging.Log(errorMessage)
+    EventScheduler.ScheduleEvent(0, "EventScheduler.GamePrint", "TargetRunData.HandleRawTargetRunData", {message = errorMessage})
 end
 
 TargetRunData.RegisterDataEvent = function()
